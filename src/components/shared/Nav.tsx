1f76ffb23830bf95841f19e1033d68edc930df9c
Liam Murray <liam@murray.to>
2023-04-13 11:44:32 -0500

    Add Search Bar To Nav

--- src/components/shared/Nav.tsx
+++ src/components/shared/Nav.tsx
@@ -1,31 +1,50 @@
-import { type FunctionComponent } from "react";
+import { type FunctionComponent, useState } from "react";
 import Link from "next/link";
 import { BsFillBasket3Fill } from "react-icons/bs";
-import { useCart } from "~/lib/cart";
+import { FaSearch } from "react-icons/fa";
+import { useRouter } from "next/router";
 
 const Nav: FunctionComponent = () => {
-	const { cart } = useCart();
+	const [searchBarValue, setSearchBarValue] = useState("");
+	const router = useRouter();
+
+	const handleSearch = () => {
+		if (searchBarValue.length > 0) {
+			router.push(`/search?q=${encodeURIComponent(searchBarValue)}`);
+		}
+	};
+
 	return (
 		<nav className="fixed z-10 w-screen">
-			<div className="mx-auto mt-2 grid h-[75px] w-full max-w-[1024px] grid-cols-2 rounded-full border-2 border-b-4 border-black bg-white px-7 py-2">
+			<div className="mx-auto mt-2 grid h-[75px] w-full max-w-[1024px] grid-cols-4 rounded-full border-2 border-b-4 border-black bg-white px-7 py-2">
 				<div className="flex items-center">
 					<Link href={"/"}>
 						<h1 className="font-calsans text-2xl font-black">Untitled</h1>
 					</Link>
 				</div>
+				<div className="text-md col-span-2 flex items-center justify-center overflow-hidden rounded-full font-sans font-bold focus-within:border focus-within:border-blue-500">
+					<input
+						onChange={(e) => setSearchBarValue(e.target.value)}
+						placeholder="A Incredible New Story..."
+						className="h-full w-[90%] rounded-l-full border-2 border-black  p-3 text-left outline-none"
+					/>
+					<button
+						onClick={() => handleSearch()}
+						className="h-full w-[10%] rounded-r-full border-2 border-l-0 border-black bg-gray-100 p-3 text-left hover:bg-gray-200"
+					>
+						<FaSearch />
+					</button>
+				</div>
 				<div className="text-md flex items-center justify-end font-sans font-bold">
-					<Link href="/login" className="ml-3 underline">
+					<Link
+						href="/login"
+						className="ml-5 h-full rounded-full border-2 border-black bg-gray-100 p-3 text-center hover:bg-gray-200"
+					>
 						Login
-					</Link>
-					<Link href="/login" className="ml-3 underline">
-						Contact
-					</Link>
-					<Link href="/login" className="ml-3 underline">
-						About Us
 					</Link>
 					<Link
 						href={"/cart"}
-						className="ml-5 aspect-square rounded-full border-2 border-black bg-gray-100 p-3 text-center hover:bg-gray-200"
+						className="ml-2 aspect-square h-full rounded-full border-2 border-black bg-gray-100 p-3 text-center hover:bg-gray-200"
 					>
 						<BsFillBasket3Fill className="text-2xl" />
 					</Link>
