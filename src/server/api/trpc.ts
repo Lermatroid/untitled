29dbe5e4dd353abe82dae1d13240e8d71038b48f
Liam Murray <liam@murray.to>
2023-04-15 20:49:43 -0600

    More Shadcn UI stuff

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -31,9 +31,9 @@ type CreateContextOptions = Record<string, never>;
  * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
  */
 const createInnerTRPCContext = (_opts: CreateContextOptions) => {
-  return {
-    prisma,
-  };
+	return {
+		prisma,
+	};
 };
 
 /**
@@ -43,7 +43,7 @@ const createInnerTRPCContext = (_opts: CreateContextOptions) => {
  * @see https://trpc.io/docs/context
  */
 export const createTRPCContext = (_opts: CreateNextContextOptions) => {
-  return createInnerTRPCContext({});
+	return createInnerTRPCContext({});
 };
 
 /**
@@ -58,17 +58,17 @@ import superjson from "superjson";
 import { ZodError } from "zod";
 
 const t = initTRPC.context<typeof createTRPCContext>().create({
-  transformer: superjson,
-  errorFormatter({ shape, error }) {
-    return {
-      ...shape,
-      data: {
-        ...shape.data,
-        zodError:
-          error.cause instanceof ZodError ? error.cause.flatten() : null,
-      },
-    };
-  },
+	transformer: superjson,
+	errorFormatter({ shape, error }) {
+		return {
+			...shape,
+			data: {
+				...shape.data,
+				zodError:
+					error.cause instanceof ZodError ? error.cause.flatten() : null,
+			},
+		};
+	},
 });
 
 /**
@@ -92,9 +92,5 @@ export const createTRPCRouter = t.router;
  * guarantee that a user querying is authorized, but you can still access user session data if they
  * are logged in.
  */
-
-const isAuthed = t.middleware(({ctx, next}) => {
-
-}))
 
 export const publicProcedure = t.procedure;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,30 @@
 import { type Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        calsans: ["Cal Sans", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	darkMode: ["class", '[data-theme="dark"]'],
+	theme: {
+		extend: {
+			fontFamily: {
+				calsans: ["Cal Sans", "sans-serif"],
+				sans: ["var(--font-sans)", ...fontFamily.sans],
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0px" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0px" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
